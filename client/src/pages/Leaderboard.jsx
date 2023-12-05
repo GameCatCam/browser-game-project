e3bc1ed89a5973bb9cd67d2ec97d8d7f44c0c369
GameCatCam <cameronrawlins11@gmail.com>
2023-12-04 21:48:45 -0500

    fixed login logic to redirect to game page

---
+++ client/src/pages/Leaderboard.jsx
@@ -0,0 +1,31 @@
+// import { useState, useEffect } from 'react'
+// import { useMutation, useQuery } from "@apollo/client";
+
+// import { GET_SCORES } from '../utils/queries'
+// import { RESET_SCORE } from '../utils/mutations'
+
+const Leaderboard = () => {
+    // Code for idk high scores or smth
+    // const getScores = async () => {
+    //     const [highScores, setHighScores] = useState([])
+    // }
+
+    // const resetScore = () => {
+    //     const [myScore, setMyScore] = useState()
+
+    //     const [resetScore] = useMutation(RESET_SCORE)
+    // }
+
+	return (
+        <>
+            <h2>
+                Leaderboard Page
+            </h2>
+            <ul>
+
+            </ul>
+        </>
+	);
+};
+
+export default Leaderboard;
