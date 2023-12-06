4e55dc819bc9bae102431fcdd747dc13575ab76c
Anthony Iacano <iacano5@outlook.com>
2023-12-05 20:07:09 -0500

    adjusted some css on the game page

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -1,8 +1,15 @@
+import logo from "../assets/img/logo.png"
+
 const Game = () => {
 	return (
 		<div>
-            Game Page
-			<canvas id="canvas"></canvas>
+			<header>
+				<img id="logo" src={logo}width="18%"></img>
+			</header>
+			<div id="main-game">
+				<h1>Game Page</h1>
+				<canvas id="canvas"></canvas>
+			</div>
 		</div>
 	);
 };
