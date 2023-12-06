b5169684312c9f1c2cb23d995ae9afd8deef38cb
Chad Smith <cds021@yahoo.com>
2023-12-05 21:23:06 -0500

    update home page styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import '../App.css';
 import Auth from '../utils/auth';
+import chef from '../assets/chef.png'
+
 
 const Home = () => {
     return (
-        <div className="flex-row px-1 container">
+        <div className="flex-row px-1 container home">
+            <div>
+                <h1 className = "title">Fancy Chef</h1>
+            </div><div>
+                <img src={chef} className='chef' />
+            </div>
             {Auth.loggedIn() ?
                 <div>
                     <a href="/" class="btn btn-primary" role="button" onClick={() => Auth.logout()}>
@@ -12,8 +19,12 @@ const Home = () => {
                 </div>
                 :
                 <div>
-                    <a href="./signup" class="btn btn-primary" role="button">Signup</a>
-                    <a href="./login" class="btn btn-secondary" role="button">Login</a>
+                    <div>
+                        <a href="./signup" class="btn btn-primary" role="button">Sign Up</a>
+                    </div>
+                    <div>
+                        <a href="./login" class="btn btn-secondary" role="button">Login</a>
+                    </div>
                 </div>
             }
         </div>
