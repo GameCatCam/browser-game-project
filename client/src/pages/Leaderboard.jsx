0accebd5708cac6a89ad282d0305dbacb227f777
GameCatCam <cameronrawlins11@gmail.com>
2023-12-05 19:00:24 -0500

    sorted user data on Leaderboard

--- client/src/pages/Leaderboard.jsx
+++ client/src/pages/Leaderboard.jsx
@@ -10,7 +10,7 @@ const Leaderboard = () => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
 
-    const users = data.allUsers;
+    const sortedUsers = [...data.allUsers].sort((a, b) => b.highScore - a.highScore);
 
 	return (
         <>
@@ -19,7 +19,7 @@ const Leaderboard = () => {
             </h2>
             <div>
                 <ul className="leader-list">
-                {users.map(user => (
+                {sortedUsers.map(user => (
                     <li className="leader-unit" key={user._id}>
                         <p className="leader-user">{user.username}</p>
                         <p className="leader-score">{user.highScore}</p>
