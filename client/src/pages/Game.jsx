155d6cd5eafa7a505c4ce7c3b6e0c6913cd50132
Anthony Iacano <iacano5@outlook.com>
2023-12-10 14:50:14 -0500

    updated syntax

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -16,18 +16,18 @@ const Game = () => {
 	const navigate = useNavigate();
 	const { loading, error, data, refetch } = useQuery(QUERY_USERS);
   
-	const handleButtonClick = () => {
-	  // Perform any necessary actions before navigating, e.g., triggering a refetch
+	const handleLeaderboardClick = () => {
+	  //freshes the leaderboard with the latest info from query_users
 	  refetch();
   
-	  // Navigate to the new page
+	  // Navigates to leaderboard
 	  navigate('/leaderboard');
 	};
 
 	return (
 		<div>
 			<header>
-				<button className="btn btn-secondary gmBtn" onClick={handleButtonClick}>Leaderboard</button>
+				<button className="btn btn-secondary gmBtn" onClick={handleLeaderboardClick}>Leaderboard</button>
 				<img id="logo" src={logo}width="18%"></img>
 				<a className="btn btn-secondary gmBtn" href="/" onClick={() => Auth.logout()}>
                     Logout
